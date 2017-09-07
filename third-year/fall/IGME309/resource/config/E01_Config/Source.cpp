49a9cccfa9a8d64477925a2e089cef0434920a93
Ian Effendi (RIT Student) <effendiian@gmail.com>
2017-09-07 11:52:13 -0400

    Added base configuration VS 2015 project.

---
+++ third-year/fall/IGME309/resource/config/E01_Config/Source.cpp
@@ -0,0 +1,16 @@
+/*  
+	Source.cpp 
+	E01 - VS 2015 Config
+	Ian Effendi
+	9/6/2017
+*/
+
+#include "Source.h"
+
+int main() {
+	std::cout << "Hello World\n";
+
+	std::cout << "Press enter to finish.";
+	getchar();
+	return 0;
+}
---
+++ third-year/fall/IGME309/resource/config/E01_Config/Source.h
@@ -0,0 +1,11 @@
+/* 
+	Source.h 
+	Ian Effendi
+*/
+
+#ifndef _SOURCE_H
+#define _SOURCE_H
+
+#include <iostream>
+
+#endif // _SOURCE_H
